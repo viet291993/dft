98c63467384677985cf63c040d75b937b8e3db50
thaidh <landd@dft.vn>
2019-10-10 17:54:01 +0700

    Dương Hồng Thái - hiển thị danh sách thông tin các nhân và thêm thông tin cá nhân

--- src/main/java/dft/domain/service/TtCaNhanService/TtCaNhanServiceImplement.java
+++ src/main/java/dft/domain/service/TtCaNhanService/TtCaNhanServiceImplement.java
@@ -1,5 +1,6 @@
 package dft.domain.service.TtCaNhanService;
 
+import dft.domain.model.DmQuanHuyen;
 import dft.domain.model.TtCaNhan;
 import dft.domain.repository.TtCaNhanRepository.TtCaNhanRepository;
 import org.springframework.stereotype.Service;
@@ -17,5 +18,35 @@ public class TtCaNhanServiceImplement implements TtCaNhanService {
     @Override
     public List<TtCaNhan> findAllTtCaNhan() {
         return ttCaNhanRepository.findAllTtCaNhan();
+    }
+
+    @Override
+    public List<DmQuanHuyen> findAllTinh() {
+        return ttCaNhanRepository.findAllTinh();
+    }
+
+    @Override
+    public List<DmQuanHuyen> findAllQuanHuyen() {
+        return ttCaNhanRepository.findAllQuanHuyen();
+    }
+
+    @Override
+    public List<DmQuanHuyen> findAllThonXom() {
+        return ttCaNhanRepository.findAllThonXom();
+    }
+
+    @Override
+    public void insert(TtCaNhan ttCaNhan) {
+        ttCaNhanRepository.insert(ttCaNhan);
+    }
+
+    @Override
+    public int findOne(String maYTeCaNhan) {
+        return ttCaNhanRepository.findOne(maYTeCaNhan);
+    }
+
+    @Override
+    public void update(TtCaNhan ttCaNhan) {
+        ttCaNhanRepository.insert(ttCaNhan);
     }
 }
